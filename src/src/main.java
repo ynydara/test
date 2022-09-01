2a7328c926d01c11db0b781d2a527f583acd303f
W0718860 <alyssa.major@selu.edu>
2022-08-31 19:49:06 -0500

    make bottles

--- src/src/main.java
+++ src/src/main.java
@@ -4,8 +4,14 @@ public class main {
             Bottle b2 = new Bottle(1000, "steel"); //yeti
             Bottle b3 = new Bottle(-1000, "dark matter");//thanos bottle
 
-            System.out.print("Water bottle volume" + b1.volume);
-            System.out.print("Water bottle material: " + b1.material);
+            System.out.println("Water bottle volume:   " + b1.volume);
+            System.out.println("Water bottle material: " + b1.material);
+
+            System.out.println("Water bottle volume:   " + b2.volume);
+            System.out.println("Water bottle material: " + b2.material);
+
+            System.out.println("Water bottle volume:   " + b3.volume);
+            System.out.println("Water bottle material: " + b3.material);
 
     }//main
 }//class
